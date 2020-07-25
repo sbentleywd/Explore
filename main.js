960c46f054865e889615a7a6508245be3fbe161d
sbentley85 <61815607+sbentley85@users.noreply.github.com>
2020-07-25 16:45:02 +1200

    first commit

---
+++ main.js
@@ -0,0 +1,119 @@
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4"), $("#venue5"), $("#venue6")];
+const $weatherDiv = $("#weather1");
+const $mapsDiv = $("#map1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var numVenues = 0
+
+
+
+// Random number functions
+
+randNums = []
+
+const generateArray = (num) => {
+  for (i=0; i<=num; i++) {
+      randNums.push(i)
+  }
+  
+};
+
+picked = []
+
+const generatePos = () => {
+  let randIndex = Math.floor(Math.random() * (randNums.length)); 
+  randNum = randNums[randIndex]
+  picked.push(randNums[randIndex])
+  randNums.splice(randIndex,1)
+  
+}
+
+
+// AJAX functions
+const getVenues = async () => {
+  picked = []
+  const location = $input.val()
+  const urlToFetch = `http://localhost:3001/attractions/${location}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      console.log(response);
+      const jsonResponse = await response.json();
+      const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+      numVenues = jsonResponse.response.groups[0].items.length;
+      console.log(jsonResponse);
+      generateArray(numVenues-1);
+      
+      for (i=0; i < numVenues; i++) {
+        generatePos()
+      }
+
+      return venues;
+    }
+    
+    
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
+
+const getForecast = async () => {
+  const location = $input.val()
+  
+  const urlToFetch = `http://localhost:3001/weather/${location}`;
+  
+  try {
+    const response = await fetch(urlToFetch)
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      
+      return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+// Render functions
+const renderVenues = (venues) => {
+  $venueDivs.forEach(($venue, index) => {
+       
+    const venue = venues[picked[index]];
+    const venueIcon = venue.categories[0].icon;
+    const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
+    let venueContent = createVenueHTML(venue.name, venue.location,venueImgSrc);
+    $venue.append(venueContent);
+  });
+  $destination.append(`<h2>${venues[0].location.city}</h2>`);
+}
+
+const renderForecast = (day) => {
+  
+  
+	let weatherContent = createWeatherHTML(day);
+  $weatherDiv.append(weatherContent);
+}
+
+const renderMaps = (location) => {
+  let mapsContent = createMapsHTML(location);
+  $mapsDiv.append(mapsContent)
+}
+
+
+const executeSearch = () => {
+  $venueDivs.forEach(venue => venue.empty());
+  $weatherDiv.empty();
+  $destination.empty();
+  $container.css("visibility", "visible");
+  getVenues().then(venues => renderVenues(venues));
+  getForecast().then(forecast => renderForecast(forecast));
+  renderMaps($input.val())
+  return false;
+}
+
+$submit.click(executeSearch)
---
+++ server/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
+const cors = require('cors');
+const path=require('path');
+require("dotenv").config({path:path.resolve(__dirname, '../.env')});
+const fetch = require('node-fetch');
+
+app.options('*', cors())
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors({
+    // allows Content-Type header to be set - not exposed by default
+    exposedHeaders: ['Content-Type']
+}));
+
+app.use(errorhandler());
+
+app.param('location', (req, res, next, location) => {
+    req.location=location;
+    next();
+})
+
+
+app.get("/weather/:location", async (req, res) => {
+    const openWeatherKey = process.env.OPENWEATHER_KEY;
+    const location = req.location
+    const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    const urlToFetch = `${weatherUrl}?&q=${location}&APPID=${openWeatherKey}`
+    
+    const response = await fetch(urlToFetch)
+    if(response.ok) {
+        
+        const jsonResponse = await response.json();
+        
+        res.json(jsonResponse)
+    }
+    
+});
+
+app.get("/attractions/:location", async (req, res) => {
+    const attractionsUrl = 'https://api.foursquare.com/v2/venues/explore?near=';
+    const clientId = process.env.FOURSQUARE_CLIENT_ID
+    const clientSecret = process.env.FOURSQUARE_CLIENT_SECRET
+    const location = req.location
+    const urlToFetch = `${attractionsUrl}${location}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20200409`
+    
+    const response = await fetch(urlToFetch)
+    if(response.ok) {
+        
+        const jsonResponse = await response.json();
+        
+        res.json(jsonResponse)
+    }
+
+
+    // res.send("Getting attractions")
+});
+
+
+
+
+
+app.listen(port, () => {
+    
+    console.log(`Server is up on port ${port}!`);
+ });
